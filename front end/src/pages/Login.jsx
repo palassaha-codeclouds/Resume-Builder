d71a128cf54d657c425ae999f84502367c6b0ca6
palassaha-codeclouds <palas.saha@codeclouds.co.in>
2025-10-26 22:41:57 +0530

    fixes before adding auth

--- front end/src/pages/Login.jsx
+++ front end/src/pages/Login.jsx
@@ -3,9 +3,9 @@ import { Lock, Mail, User2Icon } from 'lucide-react'
 
 const Login = () => {
 
-  const query = new URLSearchParams(window.location.search)
-  const urlState = query.get('state')
-  const [state, setState] = React.useState(urlState || "login")
+    const query = new URLSearchParams(window.location.search)
+    const urlState = query.get('state')
+    const [state, setState] = React.useState(urlState || "login")
 
     const [formData, setFormData] = React.useState({
         name: '',
@@ -23,9 +23,9 @@ const Login = () => {
         setFormData(prev => ({ ...prev, [name]: value }))
     }
 
-  return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-50'>
-        <form onSubmit={handleSubmit} className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
+    return (
+        <div className='flex items-center justify-center min-h-screen bg-gray-50'>
+            <form onSubmit={handleSubmit} className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
                 <h1 className="text-gray-900 text-3xl mt-10 font-medium">{state === "login" ? "Login" : "Sign up"}</h1>
                 <p className="text-gray-500 text-sm mt-2">Please {state} to continue</p>
                 {state !== "login" && (
@@ -35,11 +35,11 @@ const Login = () => {
                     </div>
                 )}
                 <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-                    <Mail sixe={13} color='#6B7280'/>
+                    <Mail sixe={13} color='#6B7280' />
                     <input type="email" name="email" placeholder="Email id" className="border-none outline-none ring-0" value={formData.email} onChange={handleChange} required />
                 </div>
                 <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-                    <Lock sixe={13} color='#6B7280'/>
+                    <Lock sixe={13} color='#6B7280' />
                     <input type="password" name="password" placeholder="Password" className="border-none outline-none ring-0" value={formData.password} onChange={handleChange} required />
                 </div>
                 <div className="mt-4 text-left text-teal-600">
@@ -50,8 +50,8 @@ const Login = () => {
                 </button>
                 <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-teal-600 hover:underline">click here</a></p>
             </form>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Login
